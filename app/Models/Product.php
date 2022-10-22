c174a16b1c7962401c8f24ba7d91f53567107854
mmmmmild <nithinard_waongwan@cmu.ac.th>
2022-10-22 09:48:16 +0700

    pd

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['code', 'name', 'price', 'description'];
+
+    function shops() {
+        return $this->belongsToMany(Shop::class);
+        }
+    
+    function type() {
+        return $this->belongsTo(Type::class);
+    }
+}
